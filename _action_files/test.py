b2ce2aaa725163970da8539984c451dd827afdc0
Hamel Husain <hamel.husain@gmail.com>
2020-02-06 18:05:42 -0800

    test

--- _action_files/test.py
+++ _action_files/test.py
@@ -1,1 +1,6 @@
-print('::set-output name=draft_url::https://5e3cc134383dcd17287281b5--fastpages.netlify.com')
+
+def show_stuff():
+    print('::set-output name=draft_url::https://5e3cc134383dcd17287281b5--fastpages.netlify.com')
+
+if __name__ == '__main__':
+    show_stuff()
