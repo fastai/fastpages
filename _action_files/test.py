482816c1ae01a8de1c3d48a6060f6b8ff38b7343
Hamel Husain <hamel.husain@gmail.com>
2020-02-06 18:08:27 -0800

    test

--- _action_files/test.py
+++ _action_files/test.py
@@ -3,4 +3,5 @@ def show_stuff():
     print('::set-output name=draft_url::https://5e3cc134383dcd17287281b5--fastpages.netlify.com')
 
 if __name__ == '__main__':
+    print('Printing something else')
     show_stuff()
