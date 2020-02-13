50870246058a492390373e89c13c6efc96c2df73
Asem <asem_alla@msn.com>
2020-02-13 08:30:13 +0200

    bypass force date in name

--- _action_files/fast_template.py
+++ _action_files/fast_template.py
@@ -13,18 +13,19 @@ def rename_for_jekyll(nb_path: Path, warnings: Set[Tuple[str, str]]=None, force_
     Return a Path's filename string appended with its modified time in YYYY-MM-DD format.
     """
     assert nb_path.exists(), f'{nb_path} could not be found.'
-
+    
     # Checks if filename is compliant with Jekyll blog posts
-    if force_date and _re_blog_date.match(nb_path.name): return nb_path.with_suffix('.md').name.replace(' ', '-')
+    if _re_blog_date.match(nb_path.name): return nb_path.with_suffix('.md').name.replace(' ', '-')
     
     else:
         clean_name = _re_numdash.sub('', nb_path.with_suffix('.md').name).replace(' ', '-')
-
+        
         if force_date:
             # Gets the file's last modified time and and append YYYY-MM-DD- to the beginning of the filename
             mdate = os.path.getmtime(nb_path) - 86400 # subtract one day b/c dates in the future break Jekyll
             dtnm = datetime.fromtimestamp(mdate).strftime("%Y-%m-%d-") + clean_name
             assert _re_blog_date.match(dtnm), f'{dtnm} is not a valid name, filename must be pre-pended with YYYY-MM-DD-'
-        # push this into a set b/c _nb2htmlfname gets called multiple times per conversion
-        if warnings: warnings.add((nb_path, dtnm))
-        return dtnm
+            # push this into a set b/c _nb2htmlfname gets called multiple times per conversion
+            if warnings: warnings.add((nb_path, dtnm))
+            return dtnm
+        else: return clean_name
