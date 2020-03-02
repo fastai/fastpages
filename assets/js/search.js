9cada97d6667f5bd76313d7534b7fe6eb2479f8f
Hamel Husain <hamel.husain@gmail.com>
2020-03-02 15:53:03 -0800

    update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -49,7 +49,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '{{ "assets/js/search-data.json" | absolute_url }}', true);
+    request.open('GET', '{{ "assets/js/search-data.json" | relative_url }}', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
