0aeeddad45d05b0bf21a24dd3f99fc76453b805f
nicolai <nicolaibthomsen@gmail.com>
2022-02-15 21:34:58 +0100

    auto doc title + better gdoc_ids format

--- _action_files/gdocs2word.py
+++ _action_files/gdocs2word.py
@@ -1,16 +1,20 @@
 """Pulls specified google documents"""
 import json
+import re
 import requests
 from pathlib import Path
 from typing import Dict
 
+
 def download_gdocs(
-    urls: Dict[str, str],
+    g_ids: Dict[str, str],
     format: str = "docx",
     dir_path: str = "_word",
     chunk_size: int = 8192,
 ):
-    """
+
+    """ Downloads specified google docs into /_word as .docx files
+
     Args:
         urls (dict): A dict of doc_name, url key value pairs of
                      e.g. "my post": "1d1N-2j56BVRVXOFPRlJRdgictahCV"
@@ -18,8 +22,9 @@ def download_gdocs(
                                 Defaults to "docx".
         dir_path (str, optional): dir in which to store downloaded file.
                                   Defaults to "_word".
-        chunk_size (int, optional): chunk sixe for reponse byte stream. 
-                                    Defaults to 8192.
+        chunk_size (int, optional): chunk sixe for reponse byte stream. D
+                                    efaults to 8192.
+
     Returns:
         int: request status code
     """
@@ -27,7 +32,7 @@ def download_gdocs(
     try:
 
         session = requests.Session()
-        for file_name, g_id in urls.items():
+        for g_id in g_ids:
 
             dl_uri = "https://docs.google.com/document/export"
             params = {"format": format, "id": g_id}
@@ -35,7 +40,12 @@ def download_gdocs(
             with session.get(dl_uri, params=params, stream=True) as r:
                 r.raise_for_status()
 
-                local_file = Path(f"{dir_path}/{file_name}.{format}")
+                # get file name from headers
+                file_name = re.findall(
+                    'filename="(.+)"', r.headers["Content-Disposition"]
+                )[0]
+
+                local_file = Path(f"{dir_path}/{file_name}")
                 with local_file.open("wb") as f:
                     for chunk in r.iter_content(chunk_size):
                         f.write(chunk)
@@ -43,16 +53,11 @@ def download_gdocs(
     except requests.HTTPError:
         pass
 
-    finally:
-        return r.status_code
-
 
 if __name__ == "__main__":
 
-    with open("_word/gdocs_named_paths.json") as j:
-        named_urls = json.load(j)
-        
-    named_urls.pop("<title>", None)
-    if named_urls:
-        download_gdocs(named_urls)
+    with Path("_word/gdoc_ids.json").open() as j:
+        gdoc_ids = json.load(j)["gdoc_ids"]
 
+    if gdoc_ids:
+        download_gdocs(gdoc_ids)
